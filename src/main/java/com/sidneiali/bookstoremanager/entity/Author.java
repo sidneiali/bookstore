db048e5d862acb5498c20dc63cd9c9bcc8c72f0a
sidneiali <sidneiali01@gmail.com>
2021-07-13 16:03:13 -0300

    feat: add entity author

---
+++ src/main/java/com/sidneiali/bookstoremanager/entity/Author.java
@@ -0,0 +1,17 @@
+package com.sidneiali.bookstoremanager.entity;
+
+import javax.persistence.*;
+
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer age;
+}
